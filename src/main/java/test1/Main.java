fbaf89d1e8efac41048650033fdba038413152dd
NetAcad DEVASC <tamalero@gmail.com>
2024-06-05 04:15:51 +0000

    added additional data to jenkinsfile and pom.xml

--- src/main/java/test1/Main.java
+++ src/main/java/test1/Main.java
@@ -6,7 +6,9 @@ import org.apache.log4j.*;
 public class Main {
     private WebDriver driver;
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "K:\\School\\SQA109 Software Test Automation 01\\Selenium Project\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); //locate chromedriver executable and instances it
+       // System.setProperty("webdriver.chrome.driver", "K:\\School\\SQA109 Software Test Automation 01\\Selenium Project\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); //locate chromedriver executable and instances it, Windows Local
+         System.setProperty("webdriver.chrome.driver", " /usr/bin/chromedriver"); //linux
+       
         WebDriver driver = new ChromeDriver(); // New chrome driver instance
             
             Navigate naviGate = new Navigate(driver);  
